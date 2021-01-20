a38a5dc696490707907d9567a612d5e9b6391b70
Anand Phulwani <portalcontrol@harlax.com>
2021-01-20 19:47:34 +0530

    Changed php path.

--- sources/libCompiler/compile.bat
+++ sources/libCompiler/compile.bat
@@ -2,5 +2,5 @@
 @echo to run you need to have php and jre installed
 @echo replace path_to_php_dir with real path below
 
-@c:\path_to_php_dir\php.exe lib_compiler.php
+@php.exe lib_compiler.php
 @pause
